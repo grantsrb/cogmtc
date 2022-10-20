b6eeb4cdc9ec8cddba0a0d61ed70c39c402b584d
Satchel Grant <grantsrb@gmail.com>
2022-10-20 10:18:41 -0700

    enable non-rendered prediction logging

--- training_scripts/watch_model.py
+++ training_scripts/watch_model.py
@@ -12,6 +12,10 @@ automatically selects the best model from the training.
 
 $ python3 watch_model.py exp_name/model_folder/
 """
+render = False
+verbose = True
+targ_range = [15,22]
+
 if __name__ == "__main__":
     model_folder = sys.argv[1]
     checkpt = cogmtc.utils.save_io.load_checkpoint(
@@ -21,9 +25,10 @@ if __name__ == "__main__":
     hyps = checkpt["hyps"]
     hyps["n_eval_steps"] = 1000
     hyps["seed"] = int(time.time())
-    hyps["render"] = True
-    model = globals()[hyps["model_type"]](**hyps).cuda()
-    model.load_state_dict(checkpt["state_dict"])
+    hyps["render"] = render
+    hyps["val_targ_range"] = targ_range
+    model = cogmtc.utils.save_io.load_model(checkpt)
+    model.cuda()
     model.eval()
     model.reset()
     val_runner = cogmtc.experience.ValidationRunner(hyps)
@@ -33,5 +38,5 @@ if __name__ == "__main__":
         print("EnvType:", env_type)
         val_runner.oracle = val_runner.oracles[env_type]
         data = val_runner.collect_data(
-            model, n_targs=None, env_type=env_type
+            model, n_targs=None, env_type=env_type, verbose=verbose
         )
